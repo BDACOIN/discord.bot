183a4fdbf92fdb3a806defe430ca427c7e89ba5b
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2018-07-10 21:55:38 +0900

    attachmentsの文字列がbase58であっても、そうでなくても、透過的に扱えるようにするための関数

---
+++ CoinSwap/Base58DecodedWalletAddress.py
@@ -0,0 +1,12 @@
+import sys
+import base58
+
+def get_base58_decoded_wallet_address(str_attachment):
+    try:
+        d58 = base58.b58decode(str_attachment)
+        uni = d58.decode("utf8")
+        return uni
+    except:
+        return str_attachment
+
+    return uni
