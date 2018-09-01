23b4a9e28de7719eb1d40b6589bfdc56215455bf
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2018-09-01 11:02:40 +0900

    確率の調整。1日10人ぐらいに収まりやすいように。

--- Diatan/JapaneseOmikuji.py
+++ Diatan/JapaneseOmikuji.py
@@ -185,12 +185,11 @@ async def get_embedded_omikuji_object(message):
     }
     
     # ハッシュからランダムで１つ選ぶ
-    omikuji_key, omikuji_lv = random.choice(list(un_list.items()))
-    rnd = random.choice([0,1,2])
-    print("rnd:" + str(rnd))
-    if rnd >= 1 and (omikuji_key == "大吉" or omikuji_key == "凶"):
-        print("ふりなおし")
-        omikuji_key, omikuji_lv = random.choice(list(un_list.items()))
+    rndstr = random.choice(["吉", "吉", "吉", "中吉", "中吉", "中吉", "末吉", "末吉", "末吉", "大吉", "凶"])
+    omikuji_key = rndstr
+    omikuji_lv = un_list[rndstr]
+    print("キ★" + omikuji_key)
+    print("値★" + omikuji_lv)
 
     # 該当のメンバーはすでにおみくじを引いているかもしれない
     id = message.author.id
@@ -210,8 +209,10 @@ async def get_embedded_omikuji_object(message):
     
     # 該当のメンバーは今日はじめておみくじを引いた
     else:
+        max_omikuji_payment_target = 10
+
         # 大吉以外で、かつ大吉を引いたメンツがまだ10人未満なら
-        if omikuji_key!="大吉" and len(result["大吉"]) < 10:
+        if omikuji_key!="大吉" and len(result["大吉"]) < max_omikuji_payment_target:
             # １つ幸運のおみくじ券を減らす
             omikuji_ticket_remain = await RegistEtherMemberInfo.decrement_one_member_omikuji_data(message, id)
             if omikuji_ticket_remain == None:
@@ -219,7 +220,11 @@ async def get_embedded_omikuji_object(message):
             else:
                 print("幸運のおみくじを1枚引いた")
                 # 再度振りなおし
-                omikuji_key2, omikuji_lv2 = random.choice(list(un_list.items()))
+
+                rndstr2 = random.choice(["吉", "吉", "中吉", "中吉", "末吉", "末吉", "大吉", "凶"])
+                omikuji_key2 = rndstr2
+                omikuji_lv2 = un_list[rndstr2]
+
                 is_use_ticket = True
                 # より良い結果が出た
                 if int(omikuji_lv2) < int(omikuji_lv):
@@ -227,7 +232,6 @@ async def get_embedded_omikuji_object(message):
                     omikuji_key = omikuji_key2
                     omikuji_lv = omikuji_lv2
 
-    
         # くじを引いたというユーザー記録を足す
         result[omikuji_key].append(id)
         save_today_omikuji_data(strdate, result)
@@ -280,7 +284,7 @@ async def get_omikuji_from_kaiwa(message, override_message = ""):
          kaiwa_utf8_byte_count = 50
 
     # 最大でも100/1000 即ち10％
-    rnd = random.randint(1, 200)
+    rnd = random.randint(1, 300)
     if rnd < kaiwa_utf8_byte_count:
         try:
             success = await RegistEtherMemberInfo.increment_one_member_omikuji_data(message, message.author.id)
