30f172400bf41a3d33fba18b4a4e5db2d6b647de
komiyannma <akitsugu-komiyama@docomo.ne.jp>
2018-10-01 01:27:17 +0900

    2018/10/01 の 0時15分ぐらいのデータ（概ね9月度評価とする)

--- Diatan/JapaneseOmikuji.py
+++ Diatan/JapaneseOmikuji.py
@@ -187,7 +187,7 @@ async def get_embedded_omikuji_object(message):
     }
     
     # ハッシュからランダムで１つ選ぶ
-    rndstr = random.choice(["吉", "吉", "吉", "中吉", "中吉", "中吉", "末吉", "末吉", "大吉", "ぴょん吉", "凶"])
+    rndstr = random.choice(["吉", "吉", "吉", "中吉", "中吉", "中吉", "末吉", "末吉", "末吉", "大吉", "ぴょん吉", "凶"])
     omikuji_key = rndstr
     omikuji_lv = un_list[rndstr]
     print("キ★" + omikuji_key)
@@ -223,7 +223,7 @@ async def get_embedded_omikuji_object(message):
                 print("幸運のおみくじを1枚引いた")
                 # 再度振りなおし
 
-                rndstr2 = random.choice(["吉", "吉", "中吉", "中吉", "末吉", "末吉", "大吉", "ぴょん吉", "凶"])
+                rndstr2 = random.choice(["吉", "吉", "吉", "中吉", "中吉", "中吉", "末吉", "末吉", "大吉", "ぴょん吉", "凶"])
                 omikuji_key2 = rndstr2
                 omikuji_lv2 = un_list[rndstr2]
 
